66398e5b44f24d519fd6c464d4189068fc74cc78
Kushal Basu <kushalbasu2@gmail.com>
2023-01-11 23:12:19 +0530

    base build

--- force-app/main/default/lwc/viewAndExportRoles/viewAndExportRoles.js
+++ force-app/main/default/lwc/viewAndExportRoles/viewAndExportRoles.js
@@ -1,16 +1,48 @@
 import { LightningElement } from "lwc";
+import getDocXML from "@salesforce/apex/UserRoleExportHelper.lwcControllertoExportasXML";
+import getDocJson from "@salesforce/apex/UserRoleExportHelper.lwcControllertoExportasJSON";
+import sendEmail from "@salesforce/apex/UserRoleExportHelper.lwcControllertoExportasEmail";
 
-export default class LightningExampleAccordionBasic extends LightningElement {
-  activeSectionMessage = "";
-
-  handleToggleSection(event) {
-    this.activeSectionMessage =
-      "Open section name:  " + event.detail.openSections;
+export default class ViewAndExportRoles extends LightningElement {
+  text =
+    "You can either load the roles in xml/json format, else you can export to your email";
+  handleClickonLoadXML() {
+    getDocXML()
+      .then((result) => {
+        this.text = result;
+      })
+      .catch((error) => {
+        this.text = error;
+      });
   }
-
-  handleSetActiveSectionC() {
-    const accordion = this.template.querySelector(".example-accordion");
-
-    accordion.activeSectionName = "C";
+  handleClickonLoadJSON() {
+    getDocJson()
+      .then((result) => {
+        this.text = result;
+      })
+      .catch((error) => {
+        this.text = error;
+      });
+  }
+  handleClickonEmailXml() {
+    sendEmail({ isXml: true })
+      .then((result) => {
+        this.text = result;
+      })
+      .catch((error) => {
+        this.text = error;
+      });
+  }
+  handleClickonEmailJSON() {
+    sendEmail({ isXml: false })
+      .then((result) => {
+        this.text = result;
+      })
+      .catch((error) => {
+        this.text = error;
+      });
+  }
+  handleClickonClear() {
+    this.text = "";
   }
 }

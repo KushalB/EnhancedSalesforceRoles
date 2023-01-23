94878dc501aba31e36b8fc01116236099ba651c6
Kushal Basu <kushalbasu2@gmail.com>
2023-01-23 21:33:30 +0530

    poc done

--- force-app/main/default/lwc/dataTreeSample/basic.js
+++ force-app/main/default/lwc/dataTreeSample/basic.js
@@ -4,7 +4,7 @@ import {
   EXAMPLES_DATA_BASIC
 } from "./sampleData";
 
-export default class DataTreeSample extends LightningElement {
+export default class TreeGridBasic extends LightningElement {
   // definition of columns for the tree grid
   gridColumns = EXAMPLES_COLUMNS_DEFINITION_BASIC;
 
